3ade46c5ea57a7f12bb9742a35c42f9b1bd706f2
Collin-Fox <131672263+Collin-Fox@users.noreply.github.com>
2023-12-05 13:35:40 -0600

    updated die class

--- src/main/java/org/example/Die.java
+++ src/main/java/org/example/Die.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import java.util.Random;
+
 public class Die {
+    Random rand = new Random();
+    public int value = rand.nextInt(6) + 1;
+    public void roll() {
+        value =  rand.nextInt(6) + 1;
+    }
 
 }
